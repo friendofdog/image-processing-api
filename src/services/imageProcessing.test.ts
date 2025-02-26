775795fb1987d2a240328c5fb68b5df1c920fbd1
Kevin Kee <kevin@netsmile.jp>
2025-02-26 15:50:16 +0900

    imageProcessing: use better tests

--- src/services/imageProcessing.test.ts
+++ src/services/imageProcessing.test.ts
@@ -1,11 +1,17 @@
 import sharp from 'sharp';
-import { processImage } from './imageProcessing'; // Update with the correct path
+import { processImage } from './imageProcessing';
 import { IMAGE_SIZE } from '../constants/image';
 
 
+const { ORIGINAL, THUMBMNAIL } = IMAGE_SIZE;
+const mockedImage = 'mocked_image';
+
+const mockResize = jest.fn();
+const mockToBuffer = jest.fn();
+
 jest.mock('sharp', () => jest.fn(() => ({
-  resize: jest.fn().mockReturnThis(),
-  toBuffer: jest.fn().mockResolvedValue(Buffer.from('mocked_image'))
+  resize: mockResize.mockReturnThis(),
+  toBuffer: mockToBuffer.mockResolvedValue(Buffer.from(mockedImage))
 })));
 
 describe('processImage', () => {
@@ -15,10 +21,27 @@ describe('processImage', () => {
     jest.clearAllMocks();
   });
 
-  it('should process image with the requested resolution', async () => {
-    const result = await processImage(mockFileContent, IMAGE_SIZE.ORIGINAL);
+  it(`when called to resize an image to ${THUMBMNAIL}
+      resizes the image to 100x100
+      and returns the image`, async () => {
+    const result = await processImage(mockFileContent, THUMBMNAIL);
 
     expect(sharp).toHaveBeenCalledWith(mockFileContent);
-    expect(result).toEqual(Buffer.from('mocked_image'));
+    expect(mockResize).toHaveBeenCalledWith(100, 100, { fit: 'inside' });
+    expect(mockToBuffer).toHaveBeenCalled();
+
+    expect(result).toEqual(Buffer.from(mockedImage));
+  });
+
+  it(`when called to resize an image to ${ORIGINAL}
+      does not resize image (resizes to nullxnull)
+      and returns the image`, async () => {
+    const result = await processImage(mockFileContent, ORIGINAL);
+
+    expect(sharp).toHaveBeenCalledWith(mockFileContent);
+    expect(mockResize).toHaveBeenCalledWith(null, null, { fit: 'inside' });
+    expect(mockToBuffer).toHaveBeenCalled();
+
+    expect(result).toEqual(Buffer.from(mockedImage));
   });
 });
