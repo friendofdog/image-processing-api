c20da33daefe22d74aff8e5c72ca94b04c60b793
Kevin Kee <kevin@netsmile.jp>
2025-02-27 14:38:00 +0900

    Add Swagger docs and UI

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { healthcheck } from '@handlers/healthcheck';
 import { handleGetJobs } from '@handlers/jobs/get';
 import { handleCreateJob } from '@handlers/jobs/post';
@@ -6,6 +8,7 @@ import { handleGetJobById } from '@handlers/jobs/{jobId}/get';
 import { handleGetJobImage } from '@handlers/jobs/{jobId}/image/get';
 import upload from '@middleware/upload';
 import { validate } from '@middleware/validator';
+import swaggerDocument from '@routes/schema/swagger.json';
 import {
   getJobsSchema,
   createJobSchema,
@@ -15,7 +18,11 @@ import {
 
 const router = express.Router();
 
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.get('/healthcheck', healthcheck);
+
 router.get('/jobs', validate(getJobsSchema), handleGetJobs);
 router.get('/jobs/:jobId', validate(getJobByIdSchema), handleGetJobById);
 router.get('/jobs/:jobId/image', validate(getJobByIdSchema), handleGetJobImage);
